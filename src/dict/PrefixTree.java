a49b6d67a90024607789e8e93be38e7aedf46ec7
Ivan <ivan.monteiro.padalko@gmail.com>
2016-03-21 23:31:44 +0100

    Updated classes (removed useless functions)

--- src/dict/PrefixTree.java
+++ src/dict/PrefixTree.java
@@ -7,10 +7,6 @@ class PrefixTree{
 		this.root = new TreeNode();
 	}
 
-	public PrefixTree(TreeNode root){
-		this.root = root;
-	}
-	
 	public TreeNode getRoot(){
 		return root;
 	}
@@ -30,43 +26,5 @@ class PrefixTree{
 			}	
 		}
 		cur.setIsWord(true);	
-	}
-
-
-	// The next two functions won't be used since the search is done always from the beginning of the tree
-	// Can't deal with non-ASCII chars, so, a lot of exceptions
-	// They stay in this code anyway only for being used as example for the next code versions
-	public boolean search(String word){
-		TreeNode cur = root;
-		for (char c: word.toCharArray()){
-			if (c == '\''){
-				if (cur.getChild(26) == null)
-					return false;
-				cur = cur.getChild(26);	
-			}
-			else{	
-				if (cur.getChild(c-'a') == null)
-					return false;
-				cur = cur.getChild(c-'a');
-			}
-		}
-		return cur.getIsWord();
-	}
-
-	public boolean prefixSearch(String prefix){
-		TreeNode cur = root;
-		for (char c: prefix.toCharArray()){
-			if (c == '\''){
-				if (cur.getChild(26) == null)
-					return false;
-				cur = cur.getChild(26);	
-			}	
-			else{
-				if (cur.getChild(c-'a') == null)
-					return false;
-				cur = cur.getChild(c-'a');
-			}
-		}
-		return true;
 	}
 }
