98d209dd7a4fe34b74b8f52cab2d3f9d3c61f0ea
Ivan <ivan.monteiro.padalko@gmail.com>
2016-03-21 23:32:21 +0100

    Implemented new functions

--- src/dict/Explorer.java
+++ src/dict/Explorer.java
@@ -7,17 +7,31 @@ class Explorer{
 	private PrefixTree tree;
 	private TreeNode explorerNode;
 	private Deque<TreeNode> stackOfNodes; // Preferable over stack in the java documentation
+	private String word;
 	private boolean possibleWord;
-	final String inputFile = "dict/words";
+	private int wrongLetters;
+	private final String inputFile = "dict/words";
 
-	// To do: implement setters and getters
+	public boolean getPossibleWord(){
+		return possibleWord;
+	}
+
+	public String getString(){
+		return word;
+	}
+
+	public int getWrongLetters(){
+		return wrongLetters;
+	}
 
 	public Explorer(){
 		tree = new PrefixTree();
 		loadDictionary();
 		explorerNode = tree.getRoot();
 		stackOfNodes = new ArrayDeque<TreeNode>(); // Not thread-safe, but, probably, this won't be a problem
-		possibleWord = true; 
+		word = "";
+		possibleWord = true;
+		wrongLetters = 0;
 	}
 
 	private void loadDictionary() throws FileNotFoundException, IOException{
@@ -31,24 +45,51 @@ class Explorer{
 	public void returnToRoot(){
 		explorerNode = tree.getRoot();
 		stackOfNodes.clear();
+		word = "";
 		isWord = true;
+		wrongLetters = 0;
 	}
 	
-	public void explore(char c){
-		if (possibleWord){
-			if (c == '\''){
-				if (explorerNode.getChild(26) == null)
-					possibleWord = false;
-				else{
-					stackOfNodes.push(explorerNode.getChild(26));
+	// Returns Integer.MAX_VALUE when the word has not finished yet
+	public int explore(char c){
+		// Deal with enter key later (I hope that the user don't use it)
+		if (c == ' '){	
+			// int out = computePoints();
+			explorerNode.increaseFrequency();
+			returnToRoot();
+			// return out;
+		}
+		else if (c == '\b'){
+			if (wrongLetters == 0)
+				explorerNode = stackOfNodes.pop();
+			else if (wrongLetters == 1){
+				wrongLetters--;
+				possibleWord = true;
+			}
+	    		    else
+				wrongLetters--;
+		}
+		else{
+			if (possibleWord){
+				stackOfNodes.push(explorerNode);
+				if (c == '\''){
+					word += c;
+					if (explorerNode.getChild(26) == null)
+						possibleWord = false;
 					explorerNode = explorerNode.getChild(26);	
 				}
-			}
-			else{ // Verify backspace and ASCII encoding	
-				if (cur.getChild(c-'a') == null)
-					return false;
-				cur = cur.getChild(c-'a');
+				else if (c >= 'a' && c <= 'z'){
+					word += c;
+					if (explorerNode.getChild(c-'a') == null)
+						possibleWord = false;
+					explorerNode = explorerNode.getChild(c-'a');
+				}
+				else{
+					wrongLetters++;
+					possibleWord = false;
+				}
 			}
 		}
+		return Integer.MAX_VALUE;
 	}
 }
